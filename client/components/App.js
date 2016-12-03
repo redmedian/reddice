48c254d96571ec06e29bf5d8245288845fe49eb7
RedMedian <info@redmedian.ru>
2016-12-03 16:39:41 +0300

    React router and basic navigation

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,14 @@
 // Наш первый компонент в клиентской части
 import React from 'react';
-import Greetings from './Greetings';
+import NavigationBar from './NavigationBar';
 
 class App extends React.Component {
   render() {
     return (
-      // <h1>Hello from react</h1>
-      <Greetings />
+      <div className="container">
+        <NavigationBar />
+        {this.props.children}
+      </div>
     );
   };
 }
