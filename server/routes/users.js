e3f1342009c055686c34dcdf4d9b3fe08169989b
RedMedian <info@redmedian.ru>
2016-12-04 03:23:28 +0300

    Adds server side validation

---
+++ server/routes/users.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import Validator from 'validator';
+import isEmpty from 'lodash/isEmpty'
+
+let router = express.Router();
+
+function validateInput(data) {
+  let errors = {};
+
+  // Validation rules
+  if (Validator.isEmpty(data.username)) {
+    errors.username = 'This field is required';
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'This field is required';
+  }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'This field is required';
+  }
+  if (Validator.isEmpty(data.passwordConfirmation)) {
+    errors.passwordConfirmation = 'This field is required';
+  }
+  if (!Validator.equals(data.password, data.passwordConfirmation)) {
+    errors.passwordConfirmation = 'Password must match';
+  }
+  if (Validator.isEmpty(data.timezone)) {
+    errors.timezone = 'This field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
+
+router.post('/', (req, res) => {
+  // console.log(req.body);
+
+  // Timeout for loading form button
+  setTimeout(() => {
+    // Validation
+    const { errors, isValid } = validateInput(req.body);
+
+    if (!isValid) {
+      res.status(400).json(errors);
+    }
+
+  }, 2000);
+});
+
+export default router;
